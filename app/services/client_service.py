97bbf4117b3e6dcb6511a8951dbbdc1b2d02de33
adrlksv <Qwerty123Andrey@bk.ru>
2025-06-06 21:29:48 +0300

    client_service

--- app/services/client_service.py
+++ app/services/client_service.py
@@ -6,7 +6,7 @@ from sqlalchemy import select
 
 from app.db.models.clients.models import Client
 from app.db.models.accounts.models import Account
-from app.db.models.transactions.models import OperationLog, OperationType
+from app.db.models.transactions.models import OperationLog, OperationType, Transaction, TransactionType
 
 
 class ClientService:
@@ -53,3 +53,100 @@ class ClientService:
         await self.session.commit()
         
         return account
+
+    async def close_account(self, account_id: int, client_id: int):
+        result = await self.session.execute(
+            select(Account)
+            .where(Account.id == account_id)
+        )
+        account = result.scalar_one_or_none()
+        
+        if not account:
+            raise ValueError(f"Account with id={account_id} not found")
+        if account.balance > 0:
+            raise ValueError(f"Account balance must be zero to close it")
+        
+        await self.session.delete(account)
+        
+        self.session.add(OperationLog(
+            client_id=client_id,
+            action=OperationType.close_account,
+            data={
+                "account_id": account_id,
+            }
+        ))
+        
+        await self.session.commit()
+        
+    async def deposit(self, account_id: int, amount: Decimal, client_id):
+        if amount <= 0:
+            raise ValueError("Amount must be greater than zero")
+        
+        result = await self.session.execute(
+            select(Account)
+            .where(Account.id == account_id)
+        )
+        
+        account = result.scalar_one_or_none()
+        
+        account.balance += amount
+        
+        transaction = Transaction(
+            from_account_id=None,
+            to_account_id=account_id,
+            amount=amount,
+            fee="0.00",
+            type=TransactionType.deposit,
+        )
+        
+        self.session.add(transaction)
+        
+        self.session.add(OperationLog(
+            client_id=client_id,
+            action=OperationType.deposit,
+            data={
+                "client_id": client_id,
+                "amount": amount,
+            }
+        ))
+        
+        await self.session.commit()
+
+    async def withdraw(self, account_id: int, amount: Decimal, client_id: int):
+        if amount <= 0:
+            raise ValueError("Amount must be greater than zero")
+        
+        result = await self.session.execute(
+            select(Account)
+            .where(Account.id == account_id)
+        )
+        
+        account = result.scalar_one_or_none()
+        
+        if not account:
+            raise ValueError(f"Account with id={account_id} not found")
+        if account.balance < amount:
+            raise ValueError("Not enough money on balance")
+        
+        account.balance -= amount
+        
+        transaction = Transaction(
+            from_account_id=account_id,
+            to_account_id=None,
+            amount=amount,
+            fee="0.00",
+            type=TransactionType.withdraw,
+        )
+        
+        self.session.add(transaction)
+        
+        self.session.add(OperationLog(
+            client_id=client_id,
+            action=OperationType.withdraw,
+            data={
+                "client_id": client_id,
+                "amount": str(amount),
+            }
+        ))
+        
+        await self.session.commit()
